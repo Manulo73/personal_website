601e097719783d52c9bdbc0639440b83062cfa7b
Juan Romero <juandumbo73@gmail.com>
2024-06-02 00:32:11 -0500

    Skills section and Nav Btns updated. No issues yet (maybe)...

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// HTML element declaration
+// Sections
+const aboutMeSection = document.getElementById("about-me-section"), projectsSection = document.getElementById("projects-section"), skillsSection = document.getElementById("skills-section"), contactSection = document.getElementById("contact-section");
+// Nav Btns
+const aboutMeBtn = document.getElementById("about-me-btn"), projectsBtn = document.getElementById("project-btn"), skillsBtn = document.getElementById("skills-btn"), contactBtn = document.getElementById("contact-btn");
+
+// Nav Btn presses
+aboutMeBtn.addEventListener("click", function () {
+    aboutMeSection.scrollIntoView();
+});
+
+projectsBtn.addEventListener("click", function () {
+    projectsSection.scrollIntoView();
+});
+
+skillsBtn.addEventListener("click", function () {
+    skillsSection.scrollIntoView();
+});
+
+contactBtn.addEventListener("click", function () {
+    contactSection.scrollIntoView();
+});
+
 // Hidden Class (Animations)
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
